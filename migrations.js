198de0c5ae1d1711bb40d82c1d07321a09a04269
John Henderson <git@j-henderson.com>
2020-08-25 17:27:41 +0000

    Initial migration with CREATE script

--- migrations.js
+++ migrations.js
@@ -3,15 +3,28 @@ const { map } = require('rxjs/operators');
 
 const driver = new neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j", "test"));
 var rxSession = driver.rxSession()
+
 rxSession
-  .readTransaction(txc =>
+  .writeTransaction(txc =>
     txc
-      .run('MATCH (person:Person) RETURN person.name AS name')
+      .run(`CREATE (a:Node { name: "A", description: "This is a description of A" }),
+            (b:Node { name: "B", description: "This is a description of B" }),
+            (c:Node { name: "C", description: "This is a description of C" }),
+            (d:Node { name: "D", description: "This is a description of D" }),
+            (b1:Node { name: "B-1", description: "This is a description of B-1" }),
+            (b2:Node { name: "B-2", description: "This is a description of B-2" }),
+            (b3:Node { name: "B-3", description: "This is a description of B-3" }),
+            (a)-[:PARENT]->(b),
+            (a)-[:PARENT]->(c),
+            (a)-[:PARENT]->(d),
+            (b)-[:PARENT]->(b1),
+            (b)-[:PARENT]->(b2),
+            (b)-[:PARENT]->(b3)`)
       .records()
       .pipe(map(record => record.get('name')))
   )
   .subscribe({
     next: data => console.log(data),
-    complete: () => console.log('completed'),
-    error: err => console.log(err)
+    complete: () => console.log('write completed'),
+    error: error => console.log(error)
   })
