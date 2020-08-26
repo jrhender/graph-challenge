753a42e27938e76560f705420abf1650f262abb8
John Henderson <git@j-henderson.com>
2020-08-26 18:37:22 +0000

    Trying to use recursive template with vue.

--- frontend/loadtree.js
+++ frontend/loadtree.js
@@ -1,23 +1,22 @@
 Vue.component('node', {
     props: ['node'],
-    template: '<li><span>{{ node.name }}</span></li>'
+    template: `<li class="node-tree">
+                <span class="label">{{ node.name }}</span>
+
+                <ul v-if="node.parent && node.parent.length">
+                  <node v-for="child in node.parent" :node="child"></node>
+                </ul>
+              </li>`
   })
 
 fetch("http://localhost:3000")
   .then(response => response.json())
-  .then(body => console.log(body))
-var app = new Vue({
-    el: '#app',
-    data: {
-      nodes: [
-        { name: 'Learn JavaScript' },
-        { name: 'Learn Vue' },
-        { name: 'Build something awesome' }
-      ]
-    }
+  .then(body => {
+    console.log(body)
+    var app = new Vue({
+        el: '#app',
+        data: {
+          nodes: body
+        }
+      })
   })
-
-async function getTree() {
-    let response = await fetch("http://localhost:3000")
-    return response
-}
