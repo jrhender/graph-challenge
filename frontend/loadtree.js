c27a5eb2e2f02c71d9b57474c07b709459a9d7f1
John Henderson <git@j-henderson.com>
2020-08-26 22:52:34 +0000

    Initial working sidebar

--- frontend/loadtree.js
+++ frontend/loadtree.js
@@ -6,17 +6,19 @@ fetch("http://localhost:3000")
     createNode(tree, "root")
   })
 
+setupToggleSidebar()
+    
 function createNode(node, parentID) {
   // add node
-  const newDiv = document.createElement("li"); 
-  const newSpan = document.createElement("span"); 
-  newDiv.appendChild(newSpan);  
-  
-  const nodeName = document.createTextNode(node.name); 
-  newSpan.appendChild(nodeName);  
-  
+  const newDiv = document.createElement("li");
+  const newSpan = document.createElement("span");
+  newDiv.appendChild(newSpan);
+
+  const nodeName = document.createTextNode(node.name);
+  newSpan.appendChild(nodeName);
+
   // add the newly created element and its content into the DOM 
-  const currentDiv = document.getElementById(parentID); 
+  const currentDiv = document.getElementById(parentID);
   currentDiv.appendChild(newDiv);
 
   // recurse through tree
@@ -24,6 +26,25 @@ function createNode(node, parentID) {
     const newUL = document.createElement("ul");
     newUL.setAttribute("id", node.name)
     newDiv.appendChild(newUL);
-    node.parent.forEach (child => createNode(child, node.name))
+    node.parent.forEach(child => createNode(child, node.name))
   }
+}
+
+function setupToggleSidebar() {
+  // Catch all the `[data-toggle-sidebar]` elements on the document.
+  document.querySelectorAll('[data-toggle-sidebar]').forEach(toggle => {
+    // Add an event listener on those elements "click" event
+    toggle.addEventListener('click', e => {
+      // get the sidebar ID from the current element data attribute
+      const sidebarID = toggle.dataset.toggleSidebar;
+      // check if there is an element on the doc with the id
+      const sidebarElement = sidebarID ? document.getElementById(sidebarID) : undefined;
+      // if there is a sidebar with the passed id (data-toggle-sidebar)
+      if (sidebarElement) {
+        // toggle the aria-hidden state of the given sidebar
+        let sidebarState = sidebarElement.getAttribute('aria-hidden');
+        sidebarElement.setAttribute('aria-hidden', sidebarState == 'true' ? false : true);
+      }
+    });
+  });
 }
