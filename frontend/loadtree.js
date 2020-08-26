27fc65ece4a03f8f093ec6eb77089c88d17e096a
John Henderson <git@j-henderson.com>
2020-08-26 23:28:22 +0000

    Got description and toggling working

--- frontend/loadtree.js
+++ frontend/loadtree.js
@@ -6,45 +6,47 @@ fetch("http://localhost:3000")
     createNode(tree, "root")
   })
 
-setupToggleSidebar()
-    
+setupCloseSidebar()
+
 function createNode(node, parentID) {
   // add node
-  const newDiv = document.createElement("li");
+  const newLi = document.createElement("li");
+  newLi.onclick = (event) => {
+    event.stopPropagation()
+    setSidebarVisibility(false)
+    setSidebarText(node.description)
+  }
   const newSpan = document.createElement("span");
-  newDiv.appendChild(newSpan);
+  newLi.appendChild(newSpan);
 
   const nodeName = document.createTextNode(node.name);
   newSpan.appendChild(nodeName);
 
   // add the newly created element and its content into the DOM 
   const currentDiv = document.getElementById(parentID);
-  currentDiv.appendChild(newDiv);
+  currentDiv.appendChild(newLi);
 
   // recurse through tree
-  if (node.parent != null) {
+  if (node.parent) {
     const newUL = document.createElement("ul");
     newUL.setAttribute("id", node.name)
-    newDiv.appendChild(newUL);
+    newLi.appendChild(newUL);
     node.parent.forEach(child => createNode(child, node.name))
   }
 }
 
-function setupToggleSidebar() {
-  // Catch all the `[data-toggle-sidebar]` elements on the document.
-  document.querySelectorAll('[data-toggle-sidebar]').forEach(toggle => {
-    // Add an event listener on those elements "click" event
-    toggle.addEventListener('click', e => {
-      // get the sidebar ID from the current element data attribute
-      const sidebarID = toggle.dataset.toggleSidebar;
-      // check if there is an element on the doc with the id
-      const sidebarElement = sidebarID ? document.getElementById(sidebarID) : undefined;
-      // if there is a sidebar with the passed id (data-toggle-sidebar)
-      if (sidebarElement) {
-        // toggle the aria-hidden state of the given sidebar
-        let sidebarState = sidebarElement.getAttribute('aria-hidden');
-        sidebarElement.setAttribute('aria-hidden', sidebarState == 'true' ? false : true);
-      }
-    });
-  });
+function setSidebarText(text) {
+  document.getElementById("sidebar_text").textContent = text;
+}
+
+function setSidebarVisibility(isHidden) {
+  const sidebarElement = document.getElementById("sidebar1");
+  if (sidebarElement) {
+    sidebarElement.setAttribute('aria-hidden', isHidden);
+  }
+}
+
+function setupCloseSidebar() {
+  const closeSideBarButton = document.getElementById("sidebar_close");
+  closeSideBarButton.onclick = () => setSidebarVisibility(true)
 }
