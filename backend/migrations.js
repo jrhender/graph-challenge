beb8e9fb4072d66b8b62a27dd03e50f8f3d39f13
John Henderson <git@j-henderson.com>
2020-08-28 17:07:28 +0000

    Added eslint to backend

--- backend/migrations.js
+++ backend/migrations.js
@@ -1,13 +1,12 @@
 const neo4j = require('neo4j-driver');
 const { map } = require('rxjs/operators');
 
-const driver = new neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j", "test"));
-var rxSession = driver.rxSession()
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'test'));
+const rxSession = driver.rxSession();
 
 rxSession
-  .writeTransaction(txc =>
-    txc
-      .run(`CREATE (a:Node { name: "A", description: "This is a description of A" }),
+  .writeTransaction((txc) => txc
+    .run(`CREATE (a:Node { name: "A", description: "This is a description of A" }),
             (b:Node { name: "B", description: "This is a description of B" }),
             (c:Node { name: "C", description: "This is a description of C" }),
             (d:Node { name: "D", description: "This is a description of D" }),
@@ -20,11 +19,10 @@ rxSession
             (b)<-[:CHILD]-(b1),
             (b)<-[:CHILD]-(b2),
             (b)<-[:CHILD]-(b3)`)
-      .records()
-      .pipe(map(record => record.get('name')))
-  )
+    .records()
+    .pipe(map((record) => record.get('name'))))
   .subscribe({
-    next: data => console.log(data),
+    next: (data) => console.log(data),
     complete: () => console.log('write completed'),
-    error: error => console.log(error)
-  })
+    error: (error) => console.log(error),
+  });
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,39 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const neo4j = require('neo4j-driver');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const dbHost = process.argv[2] // Should be the first command line arg
-const driver = new neo4j.driver(`neo4j://${dbHost}:7687`, neo4j.auth.basic("neo4j", "test"));
+const dbHost = process.argv[2]; // Should be the first command line arg
+const driver = neo4j.driver(`neo4j://${dbHost}:7687`, neo4j.auth.basic('neo4j', 'test'));
 
 const corsOptions = {
-  origin: 'http://localhost:8080'
-}
+  origin: 'http://localhost:8080',
+};
 
 app.get('/', cors(corsOptions), async (req, res, next) => {
-  const session = driver.session()
+  const session = driver.session();
   try {
-    const rootNodes = await session.run(`MATCH (n) WHERE NOT (n)-->() RETURN n`)
-    const rootNodeName = rootNodes.records[0].toObject().n.properties.name // Assuming that only a single tree is wanted
+    const rootNodes = await session.run('MATCH (n) WHERE NOT (n)-->() RETURN n');
+
+    // Assuming that only a single tree is wanted
+    const rootNodeName = rootNodes.records[0].toObject().n.properties.name;
+
     const tree = await session
-                        .run(`MATCH p=(n:Node {name:'${rootNodeName}'})<-[:CHILD*]-(m)
+      .run(`MATCH p=(n:Node {name:'${rootNodeName}'})<-[:CHILD*]-(m)
                               WITH COLLECT(p) AS ps
                               CALL apoc.convert.toTree(ps) yield value
-                              RETURN value`)
-    const json = JSON.stringify(tree.records[0].toObject().value)
-    res.json(json)
+                              RETURN value`);
+    const json = JSON.stringify(tree.records[0].toObject().value);
+    res.json(json);
+  } catch (error) {
+    next(error);
+  } finally {
+    session.close();
   }
-  catch(error) {
-    return next(error)
-  }
-  finally {
-    session.close()
-  }
-})
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
