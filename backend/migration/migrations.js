46c8164b3c8dfcfe1a50d3129aca712ee3cb465e
John Henderson <jrhender@users.noreply.github.com>
2020-08-28 16:40:30 -0700

    Actually removing last char from query
    Neo4j can't have trailing comma in query

--- backend/migration/migrations.js
+++ backend/migration/migrations.js
@@ -13,7 +13,7 @@ function generateCREATEquery() {
       relationshipCreation += `(${parent})<-[:CHILD]-(${neo4jName}),`;
     }
   });
-  return `CREATE ${nodeCreation}${relationshipCreation}`.split(0, -1);
+  return `CREATE ${nodeCreation}${relationshipCreation}`.slice(0, -1);
 }
 
 async function main() {
