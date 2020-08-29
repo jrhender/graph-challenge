ccb837a39469b4f937a7275d57082af53ca2070a
John Henderson <john.henderson@shareandcharge.com>
2020-08-29 10:47:01 -0700

    vue unit test setup seems to be working

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -25,5 +25,14 @@ module.exports = {
         jest: true,
       },
     },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
   ],
 };
